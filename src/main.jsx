804a97de207323d9b560414e6019f02e99535097
Preet Singh <preet10syd@gmail.com>
2025-08-01 16:47:40 +1000

    edit: vite config and package.json config

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Game from './Game.jsx'
+import Game from './Game'
 createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<Game />
-	</StrictMode>,
+  <StrictMode>
+    <Game />
+  </StrictMode>,
 )
